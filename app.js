9f574757f3e08be032156bf7abb359b1920db603
MartijnNieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2016-05-24 20:19:13 +0200

    added the three pages to the server

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var desks = require('./routes/desks');
+var calendar = require('./routes/calendar');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/desks', desks);
+app.use('/calendar', calendar);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
