3c53b361e71eb1b51fbad41f55f298f5bde356c7
MartijnNieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2016-05-30 12:02:08 +0200

    Prototype the data

---
+++ routes/modules/googleCalendar.js
@@ -0,0 +1,25 @@
+// client-id: 		304447379507-3e1i3dr0bn742ctcr7qphghj96182iu8.apps.googleusercontent.com
+// client-secret: 	tziovtjWbb2bkDZzTD7h_F-9
+
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var fs = require('fs');
+var readline = require('readline');
+var googleCalendar = {};
+
+googleCalendar.getAcces = function() {
+	
+}
+
+googleCalendar.apiCall = function(url) {
+
+	// make the call
+
+};
+googleCalendar.filterData = function(url) {
+
+	// make the call
+
+};
+
+module.exports = googleCalendar;
